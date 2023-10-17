ab410a9d85c7ccc8efcbf67f56e1e6065c12caae
laura.chagas <laura.chagas@zup.com.br>
2023-10-17 13:58:42 -0300

    Initial commit

---
+++ zupProgress/src/main/java/com/zup/zupProgress/ZupProgressApplication.java
@@ -0,0 +1,13 @@
+package com.zup.zupProgress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupProgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupProgressApplication.class, args);
+	}
+
+}
---
+++ zupProgress/src/test/java/com/zup/zupProgress/ZupProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.zupProgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
