6e14ffe97dacf310f4f5564e6c31b51fd420f42b
AnaJuliaSilverio <ana.silverio@zup.com.br>
2023-10-17 15:25:22 -0300

    ✨ feat: Mentor Features

--- zupProgress/src/main/java/com/zup/zupProgress/services/MentorService.java
+++ zupProgress/src/main/java/com/zup/zupProgress/services/MentorService.java
@@ -1,4 +1,16 @@
 package com.zup.zupProgress.services;
 
+import com.zup.zupProgress.model.MentorModel;
+import com.zup.zupProgress.repositories.MentorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MentorService {
+    @Autowired
+    private MentorRepository mentorRepository;
+
+    public MentorModel createMentor(MentorModel mentorModel){
+        return mentorRepository.save(mentorModel);
+    }
 }
