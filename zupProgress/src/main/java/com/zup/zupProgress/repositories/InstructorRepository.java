b3a01cb3b2ea1e0eb6e44f91e8ad36ccb0a3ba8f
AnaJuliaSilverio <ana.silverio@zup.com.br>
2023-11-01 11:36:59 -0300

    ✨ feat: Adicionando service,controller e dto de instrutor

--- zupProgress/src/main/java/com/zup/zupProgress/repositories/InstructorRepository.java
+++ zupProgress/src/main/java/com/zup/zupProgress/repositories/InstructorRepository.java
@@ -2,8 +2,12 @@ package com.zup.zupProgress.repositories;
 
 import com.zup.zupProgress.model.InstructorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InstructorRepository extends JpaRepository<InstructorModel,Long> {
+
 }
